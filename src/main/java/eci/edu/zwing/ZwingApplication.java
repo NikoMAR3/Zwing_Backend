811b7b3bf9be9b2cdc1d00c9cef185e4a1778c25
Nikolas <nikolas.martinezrive6@mail.escuelaing.edu.co>
2026-03-21 22:50:10 -0500

    Initial commit

---
+++ src/main/java/eci/edu/zwing/ZwingApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.zwing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwingApplication.class, args);
+	}
+
+}
